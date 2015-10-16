dbc4ea6531ac520ca5b8d0307eae4eaee4b9a81b
Juergen Dufner <juergen@jdufner.de>
2015-10-16 22:02:29 +0200

    Dekorierer aufrufen.

--- src/test/java/de/jdufner/experiments/proxypatterns/dynamicproxy/DekoriererTest.java
+++ src/test/java/de/jdufner/experiments/proxypatterns/dynamicproxy/DekoriererTest.java
@@ -1,5 +1,7 @@
 package de.jdufner.experiments.proxypatterns.dynamicproxy;
 
+import java.lang.reflect.Proxy;
+
 import org.junit.Test;
 
 public class DekoriererTest {
@@ -9,7 +11,8 @@ public class DekoriererTest {
     Schnittstelle implementierung = new Implementierung("test");
     Schnittstelle dekorierer = Dekorierer.buildProxy(implementierung);
     dekorierer.m1("bla");
-    ((Dekorierer) dekorierer).m2();
+    //((Dekorierer) dekorierer).m2();
+    ((Dekorierer) Proxy.getInvocationHandler(dekorierer)).m2();
   }
 
 }
